6bc269b500f901c68d1d26cd76afe8f93f5d60bb
Filip Czaplicki <filipczaplicki@gmail.com>
2016-10-10 14:35:26 +0200

    ptest almost done

--- spoj/ptest.asm
+++ spoj/ptest.asm
@@ -7,13 +7,16 @@ KERNEL_CALL equ 0x80
 STDIN equ 0
 STDOUT equ 1
 ASCII_ZERO equ 48
+ASCII_NEWLINE equ 10
+ZERO equ 0
 TEN equ 10
 
 section .data
 	input db 0
 	partial db 0
-	result db 0
-	result_str db `000\n\0` ; `` for escaping characters in C-style
+	result dw 0
+	result_len db 4
+	result_str db `0004` ; `` for escaping characters in C-style
 
 section .text
 
@@ -22,31 +25,59 @@ _start:
 	; call one_number
 	call print_result
 	call exit
-	
+
 one_number:
-    mov byte[partial], 0 ; clear partial result
-    
-    call read
-    mov ah, byte[partial]
-    imul ax, 10
-    mov bh, byte[input]
-    sub bh, ASCII_ZERO
-    add ah, bh
-    
+  mov byte[partial], 0 ; clear partial result
+
+	call read
+  mov ah, byte[partial]
+  imul ax, TEN
+  mov bh, byte[input]
+  sub bh, ASCII_ZERO
+  add ah, bh
+
+	mov byte[result], ah
 	ret
-	
+
 print_result:
-	mov byte[result], 123 ; fake data
-	mov al, 22
-	mov ah, 0
-	mov bl, 10
+	mov ax, word[result]
+	xor edx, edx
+	mov dl, 0
+
+print_result_loop:
+	cmp al, 0
+	jbe print_result_end ; unsigned jle
+	mov ah, ZERO
+	mov bl, TEN
 	div bl
-	
-	mov cl, byte[result_str]
-	add cl, al
-	mov byte[result_str], cl
-	
+
+	mov cl, byte[result_str + edx]
+	add cl, ah
+	mov byte[result_str + edx], cl
+
+	inc dl
+	jmp print_result_loop
+
+print_result_end:
+	cmp dl, 0
+	jne not_zero
+	inc dl
+not_zero:
+	mov byte[result_str + edx], ASCII_NEWLINE
+	inc dl
+	mov byte[result_len], dl
+	call reverse
 	call write
+
+reverse:
+	mov al, byte[result_str]
+	xor edx, edx
+	mov dl, byte[result_len]
+	sub dl, 2
+	mov bl, byte[result_str + edx]
+	mov byte[result_str], bl
+	mov byte[result_str + edx], al
+	ret
 
 read:
 	mov eax, SYS_READ ; read()
@@ -55,15 +86,16 @@ read:
 	mov	edx, 1 ; len
 	int KERNEL_CALL
 	ret
-	
+
 write:
 	mov eax, SYS_WRITE ; write()
 	mov ebx, STDOUT ; file
 	mov	ecx, result_str ; char *
-	mov	edx, 5 ; len
+	xor edx, edx
+	mov	dl, byte[result_len] ; len
 	int KERNEL_CALL
 	ret
-	
+
 exit:
 	mov	eax, SYS_EXIT ; exit()
 	xor	ebx, ebx ; return 0;
