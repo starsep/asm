5a1ff04508467d765a05f071d6c1c815c0dfaad6
Filip Czaplicki <filipczaplicki@gmail.com>
2016-11-20 23:53:09 +0100

    Task 1: set_channel in asm

--- zad1/enlight.asm
+++ zad1/enlight.asm
@@ -4,6 +4,7 @@ section .data
   red dq 0
   green dq 0
   blue dq 0
+  channel dq 0
   N dd 0
   M dd 0
   change dd 0
@@ -19,4 +20,24 @@ enlight:
   mov dword[change], r9d
   mov al, byte[rsp + 8]
   mov byte[delta], al
+  call set_channel
+  mov rax, qword[channel]
+  ret
+
+set_channel:
+  mov eax, dword[change]
+  cmp eax, 1
+  jne not_1
+  mov rax, qword[red]
+  mov qword[channel], rax
+  ret
+not_1:
+  cmp eax, 2
+  jne not_2
+  mov rax, qword[green]
+  mov qword[channel], rax
+  ret
+not_2:
+  mov rax, qword[blue]
+  mov qword[channel], rax
   ret
