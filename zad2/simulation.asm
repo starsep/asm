2da32286727f0105e7aa25f201369b0294057c88
Filip Czaplicki <filipczaplicki@gmail.com>
2016-12-13 23:55:07 +0100

    Task 2: assign_arguments in asm.

--- zad2/simulation.asm
+++ zad2/simulation.asm
@@ -1,8 +1,26 @@
 global start
 global step
 
+section .data
+  n dd 0 ; wysokość
+  m dd 0 ; szerokość
+  M dq 0 ; wskaźnik do danych
+  G dq 0 ; wskaźnik do grzejników
+  C dq 0 ; wskaźnik do chłodnic
+  ratio dd 0.0 ; współczynnik (float), waga
+
 section .text
 start:
+  call assign_arguments
+  ret
+
+assign_arguments:
+  mov dword[n], edi
+  mov dword[m], esi
+  mov qword[M], rdx
+  mov qword[G], rcx
+  mov qword[C], r8
+  movss dword[ratio], xmm0
   ret
 
 step:
