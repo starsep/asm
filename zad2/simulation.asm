2a1bda3d40c08a64ac404e3aea4b0d719638c11c
Filip Czaplicki <filipczaplicki@gmail.com>
2016-12-14 12:47:13 +0100

    Task 2: size, alloc/clean_matrices.

--- zad2/simulation.asm
+++ zad2/simulation.asm
@@ -1,5 +1,11 @@
+extern malloc
+extern free
+
 global start
 global step
+
+; stałe
+SIZE_OF_FLOAT equ 4
 
 section .data
   n dd 0 ; wysokość
@@ -8,19 +14,86 @@ section .data
   G dq 0 ; wskaźnik do grzejników
   C dq 0 ; wskaźnik do chłodnic
   ratio dd 0.0 ; współczynnik (float), waga
+  size dd 0 ; wielkość macierzy, na których będę robić SSE
+  result_matrix dq 0 ; wskaźnik do macierzy, w której będzie wynik
+  ratio_matrix dq 0 ; wskaźnik do macierzy, która będzie = ratio * oryginalna
+  left_matrix dq 0 ; wskaźnik do macierzy (przesunięta ratio_matrix w lewo)
+  right_matrix dq 0 ; wskaźnik do macierzy (przesunięta ratio_matrix w prawo)
+  up_matrix dq 0 ; wskaźnik do macierzy (przesunięta ratio_matrix w górę)
+  down_matrix dq 0 ; wskaźnik do macierzy (przesunięta ratio_matrix w dół)
 
 section .text
 start:
   call assign_arguments
+  call calculate_size
+  call alloc_matrices
+  call clean_matrices
   ret
 
 assign_arguments:
+  ; przypisujemy argumenty
   mov dword[n], edi
   mov dword[m], esi
   mov qword[M], rdx
   mov qword[G], rcx
   mov qword[C], r8
   movss dword[ratio], xmm0
+  ret
+
+calculate_size:
+  ; liczymy size = (n + 2) * (m + 2)
+  mov eax, dword[n]
+  add eax, 2
+  mov r9d, dword[m]
+  add r9d, 2
+  imul eax, r9d
+  mov dword[size], eax
+  ret
+
+alloc_matrices:
+  ; rdi = size * sizeof(float)
+  xor rdi, rdi
+  mov edi, dword[size]
+  imul edi, SIZE_OF_FLOAT
+  ; result_matrix = malloc(size * sizeof(float));
+  call malloc
+  mov qword[result_matrix], rax
+  ; ratio_matrix = malloc(size * sizeof(float));
+  call malloc
+  mov qword[ratio_matrix], rax
+  ; left_matrix = malloc(size * sizeof(float));
+  call malloc
+  mov qword[left_matrix], rax
+  ; right_matrix = malloc(size * sizeof(float));
+  call malloc
+  mov qword[right_matrix], rax
+  ; up_matrix = malloc(size * sizeof(float));
+  call malloc
+  mov qword[up_matrix], rax
+  ; down_matrix = malloc(size * sizeof(float));
+  call malloc
+  mov qword[down_matrix], rax
+  ret
+
+clean_matrices:
+  ; free(result_matrix)
+  mov rdi, qword[result_matrix]
+  call free
+  ; free(ratio_matrix)
+  mov rdi, qword[ratio_matrix]
+  call free
+  ; free(left_matrix)
+  mov rdi, qword[left_matrix]
+  call free
+  ; free(right_matrix)
+  mov rdi, qword[right_matrix]
+  call free
+  ; free(up_matrix)
+  mov rdi, qword[up_matrix]
+  call free
+  ; free(down_matrix)
+  mov rdi, qword[down_matrix]
+  call free
   ret
 
 step:
