c9d34ad71d1775cc62a49218b162a837e86c3ff3
Filip Czaplicki <filipczaplicki@gmail.com>
2016-12-26 13:40:49 +0100

    Task 3: c_grayscale fixed.

--- zad3/c_grayscale.c
+++ zad3/c_grayscale.c
@@ -2,8 +2,10 @@ typedef unsigned char uchar;
 
 void grayscale(int size, int maxi, const uchar *in, uchar *out,
   uchar red, uchar green, uchar blue, int p2_maxi) {
+  int sum = red + green + blue;
+  p2_maxi++;
   for (int i = 0; i < size; i++) {
     int r = in[3 * i], g = in[3 * i + 1], b = in[3 * i + 2];
-    out[i] = (red * r + green * g + blue * b) / p2_maxi % p2_maxi;
+    out[i] = (red * r + green * g + blue * b) / sum % p2_maxi;
   }
 }
