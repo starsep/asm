f12a76e7f8d7a65eb2ebde9b820269ff66e6a941
Filip Czaplicki <filipczaplicki@gmail.com>
2017-01-03 21:30:06 +0100

    Task 3: ARM asm gives good results.

--- zad3/grayscale.asm
+++ zad3/grayscale.asm
@@ -1,11 +1,9 @@
 .data
 
 .balign 4
-maxi: .word 0
 red: .word 0
 green: .word 0
 blue: .word 0
-p2_maxi: .word 0
 
 .text
 .global grayscale
@@ -13,36 +11,77 @@ p2_maxi: .word 0
 
 grayscale:
 assign:
-  ldr ip, addr_maxi
-  str r1, [ip]
+  /* zapisujemy w red argument przekazany w r3 */
   ldr ip, addr_red
-  str r1, [ip]
+  str r3, [ip]
+  /* zapisujemy w green argument przekazany na wierzchu stosu */
   ldr ip, addr_green
-  str r1, [ip]
+  ldmfd r13!, {r3}
+  str r3, [ip]
+  /* zapisujemy w blue argument przekazany jako drugi na stosie */
   ldr ip, addr_blue
-  str r1, [ip]
-  ldr ip, addr_p2_maxi
-  str r1, [ip]
+  ldmfd r13!, {r3}
+  str r3, [ip]
+  /* wrzucamy na stos rejestry, aby móc je potem przywrócić */
+  stmfd r13!, {r4-r8}
+  /* zapisujemy dane w rejestrach, skoro są już wolne */
+  /* red do r3 */
+  ldr ip, addr_red
+  ldr r3, [ip]
+  /* green do r4 */
+  ldr ip, addr_green
+  ldr r4, [ip]
+  /* blue do r5 */
+  ldr ip, addr_blue
+  ldr r5, [ip]
+  /* Założenia:
+    w r0 trzymamy counter stosu, początkowo size przekazany jako argument
+    w r1 trzymamy wskaźnik na aktualne dane wejściowe
+    w r2 trzymamy wskaźnik na wyjście
+    w r3, r4 oraz r5: red, blue, green
+    r6, r7, r8 to scratch rejestry do obliczeń */
 grayscale_loop:
-  ldr r1, [r2]
+  /* do r6 ładujemy wartość czerwonego kolejnego piksela */
+  ldr r6, [r1]
+  /* mnożymy przez współczynnik czerwonego */
+  mul r6, r3, r6
+  /* inkrementujemy wskaźnik na wejście */
+  add r1, r1, #1
+  /* do r7 ładujemy wartość zielonego kolejnego piksela */
+  ldr r7, [r1]
+  /* mnożymy przez współczynnik zielonego */
+  mul r7, r4, r7
+  /* inkrementujemy wskaźnik na wejście */
+  add r1, r1, #1
+  /* do r8 ładujemy wartość niebieskiego kolejnego piksela */
+  ldr r8, [r1]
+  /* mnożymy przez współczynnik niebieskiego */
+  mul r8, r5, r8
+  /* inkrementujemy wskaźnik na wejście */
+  add r1, r1, #1
+  /* dodajemy wyniki z niebieskiego i zielonego do czerwonego */
+  add r6, r6, r7
+  add r6, r6, r8
+  /* tutaj jeszcze możnaby zrobić dzielenie przez (red + green + blue),
+    ale *założenie* ich suma = 256. czyli nawet nie trzeba przesuwać. */
+  /* zapisujemy wynik (z r6) na wyjście */
+  str r6, [r2]
+  /* inkrementujemy wskaźnik na wyjście */
   add r2, r2, #1
-  ldr ip, [r2]
-  add r1, r1, ip
-  add r2, r2, #1
-  ldr ip, [r2]
-  add r1, r1, ip
-  add r2, r2, #1
-  ldr ip, maxi
-  @sdiv r1, r1, ip
-  str r1, [r3]
+  /* zmniejszamy counter pętli */
   sub r0, r0, #1
-  add r3, r3, #1
+  /* sprawdzamy czy już wyzerowaliśmy r0 */
   cmp r0, #0
+  /* jeżeli nie to powtarzamy */
   bne grayscale_loop
+  /* przywracamy rejestry */
+  ldmfd r13!, {r4-r8}
+  /* wrzucamy na stos rejestry, aby wierzchołek był w tym samym miejscu */
+  stmfd r13!, {r0-r1}
+  /* wracamy do C */
   bx lr
 
-addr_maxi: .word maxi
+/* adresy (Global Object Table) */
 addr_red: .word red
 addr_green: .word green
 addr_blue: .word blue
-addr_p2_maxi: .word p2_maxi
