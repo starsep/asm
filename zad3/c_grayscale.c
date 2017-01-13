acb970191ada8c03d337696003b05fb47f475def
Filip Czaplicki <filipczaplicki@gmail.com>
2017-01-13 01:06:46 +0100

    Task 3: c_grayscale fixed (out divided by 256).

--- zad3/c_grayscale.c
+++ zad3/c_grayscale.c
@@ -7,8 +7,8 @@ void grayscale(int size, const uchar *in, uchar *out, uchar red,
                uchar green, uchar blue) {
   for (int i = 0; i < size; i++) {
     int r = in[3 * i], g = in[3 * i + 1], b = in[3 * i + 2];
-    out[i] = red * r + green * g + blue * b;
-    // tutaj jeszcze możnaby zrobić / (red + green + blue),
+    out[i] = (red * r + green * g + blue * b) >> 8;
+    // tutaj możnaby zrobić / (red + green + blue),
     // ale *założenie* ich suma = 256.
   }
 }
